04b930aa2656be9808158a3d173d5dfe2f840e5f
Aryan Salaria <arayn.32@gmail.com>
2025-01-26 23:02:21 +0530

    LeetMissinNumber Using Sorting approach

---
+++ CP/leetmissingnumber.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+       sort(nums.begin(),nums.end());
+
+       int sizer = nums.size();
+
+       int fnum = 0;
+
+       if(nums[0]!=0)
+       {
+        fnum = 0;
+       }
+
+       else if(nums[sizer-1]!= sizer)
+       {
+        fnum = sizer;
+       }
+
+       else
+       {
+        for(int i=1 ; i<nums.size() ; i++)
+        {
+            if(i!= nums[i])
+            {
+                fnum = i;
+                break;
+            }
+        }
+       }
+
+       return fnum;
+    }
+
+};
