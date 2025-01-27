729cbf9adb6373f5e552afb394ac4e29987cc290
Aryan Salaria <arayn.32@gmail.com>
2025-01-27 23:09:24 +0530

    Mandom 1

---
+++ C&C++Basics/mandom.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Mandom
+{
+    public:
+
+    string fname;
+    int age;
+
+
+    void display()
+    {
+        cout << "Mandom Name is: " << fname << endl;
+        cout << "Mandom Age is: " << age << endl;
+    }
+}
+
+
+int main()
+{
+    
+}
