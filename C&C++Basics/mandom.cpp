6e5c2184bcd02bcf63b01f8e5708b00097250db5
Aryan Salaria <arayn.32@gmail.com>
2025-01-27 23:12:54 +0530

    Mandom Constructor

--- C&C++Basics/mandom.cpp
+++ C&C++Basics/mandom.cpp
@@ -9,16 +9,31 @@ class Mandom
     string fname;
     int age;
 
+    // Parameterized Constructor
+
+    Mandom(string name, int age)
+    {
+        fname = name;
+        age = age;
+    }
+
 
     void display()
     {
         cout << "Mandom Name is: " << fname << endl;
         cout << "Mandom Age is: " << age << endl;
     }
-}
 
+    void getinfo()
+    {
+        getline(cin, fname);
+        cin >> age;
+    }
+};
 
 int main()
 {
-    
+    Mandom man1 = new Mandom('Aryan',25);
+
+
 }
