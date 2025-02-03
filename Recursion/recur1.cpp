a06a7daed070995dcd42808537395c60eb36500e
Aryan Salaria <arayn.32@gmail.com>
2025-02-03 20:30:59 +0530

    Different Types of Recursive Calls

---
+++ Recursion/recur1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void fun1(int n)
+{
+
+    if(n>0)
+    {
+        cout << n << endl;
+        fun1(n-1);
+    }
+}
+
+void fun2(int n)
+{
+    if(n>0)
+    {
+        fun2(n-1);
+        cout << n << endl;
+    }
+
+}
+
+
+int main()
+{
+
+    int n = 7;
+
+    fun1(n);
+
+    cout << "--------" << endl;
+
+    fun2(n);
+}
