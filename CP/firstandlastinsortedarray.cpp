cc3295f5b31a549f8d033e927296d94e9e90957b
Aryan Salaria <arayn.32@gmail.com>
2025-02-23 23:05:00 +0530

    Better First and last Index using Binary Search

--- CP/firstandlastinsortedarray.cpp
+++ CP/firstandlastinsortedarray.cpp
@@ -1,48 +1,68 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-
-        vector <int> positions;
-
-        int start = -1;
-        int end = -1;
-        for(int i=0 ; i<nums.size() ; i++)
-        {
-            if(nums[i] == target)
-            {
-                start = i;
-
-                for(int j=i; j<nums.size() ; j++)
-                {
-                    if(nums[j] == target)
-                    {
-                        end = j;
-                    }
-                }
-
-                break;
-            }
-        }
-
-        if(start != -1 && end != -1)
-        {
-           positions.push_back(start);
-           positions.push_back(end);
-
-            return positions;
-        }
-
-        else
-        {
-            positions.push_back(-1);
-            positions.push_back(-1);
-            return positions;
-        }
-        
-    }
-};
+    public:
+      vector<int> find(vector<int>& arr, int x) {
+          // code here
+          
+          // first occurence
+          
+          int low = 0;
+          int high = arr.size()-1;
+          
+          int find = INT_MAX;
+          int lind = INT_MIN;
+          
+          while(low<=high)
+          {
+              int mid = (low+high)/2;
+              
+              if(arr[mid] == x)
+              {
+                  find = min(find,mid);
+                  high = mid -1;
+              }
+              
+              else if(arr[mid]>=x)
+              {
+                  high = mid -1;
+              }
+              else
+              {
+                  low = mid +1;
+              }
+          }
+          
+          low = 0;
+          high = arr.size()-1;
+          
+          while(low<=high)
+          {
+              int mid = (low+high)/2;
+              
+              if(arr[mid]==x)
+              {
+                  lind = max(lind,mid);
+                  low = mid +1;
+              }
+              
+              else if(arr[mid]<=x)
+              {
+                  low = mid +1;
+              }
+              else
+              {
+                  high = mid -1;
+              }
+          }
+          
+          // X NOT FOUND
+          if(find == INT_MAX && lind == INT_MIN)
+          {
+              return {-1,-1};
+          }
+          
+          return {find,lind};
+      }
+  };
