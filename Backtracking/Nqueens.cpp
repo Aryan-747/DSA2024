42c091d5c08524c33a47147c3c7d72cb45b4713c
Aryan Salaria <arayn.32@gmail.com>
2025-02-28 22:54:33 +0530

    N-Queens

---
+++ Backtracking/Nqueens.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void solve(int col, vector<vector<string>> &ans, vector<string> &board ,vector<int> &rowleft, vector<int> &lowerdiag, vector<int> &upperdiag, int n)
+{
+
+    if(col == n)
+    {
+        ans.push_back(board);
+        return;
+    }
+
+    for(int row = 0 ; row<n ; row++)
+    {
+
+        if(rowleft[row] == 0 && lowerdiag[row+col] == 0 && upperdiag[(n-1) + (col-row)] == 0)
+        {
+            board[row][col] = 'Q';
+            rowleft[row] = 1;
+            lowerdiag[row+col] = 1;
+            upperdiag[(n-1)+(col-row)] = 1;
+            solve(col+1,ans,board,rowleft,lowerdiag,upperdiag,n);
+            board[row][col] = '.';
+            rowleft[row] = 0;
+            lowerdiag[row+col] = 0;
+            upperdiag[(n-1)+(col-row)] =0;
+        }
+
+    }
+
+}
+
+int main()
+{
+    int n = 4;
+    int col = 0;
+
+
+    vector<vector<string>> ans;
+    vector<string> board(n);
+    string s(n,'.');
+
+    for(int i=0 ; i<n ; i++)
+    {
+        board[i] = s;
+    }
+
+    vector<int> rowleft(n,0), lowerdiag(2*n-1,0), upperdiag(2*n-1,0);
+
+
+    solve(col,ans,board,rowleft,lowerdiag,upperdiag,n);
+
+    for(int i=0 ; i<ans.size() ; i++)
+    {
+        for(int j=0 ; j<ans[i].size(); j++)
+        {
+            cout << ans[i][j] << '\n';
+        }
+
+        cout << endl;
+    }
+
+
+
+}
