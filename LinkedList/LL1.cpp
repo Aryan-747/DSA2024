19bb295ed03a256bad5388f84c14588de70dc1c5
Aryan Salaria <arayn.32@gmail.com>
2025-03-12 22:12:09 +0530

    LL1

---
+++ LinkedList/LL1.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Node
+{
+    public:
+
+    int data;
+    Node* next;
+
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+};
+
+
+Node* ConvertArrtoLL(vector<int> &arr)
+{
+    Node* head =  new Node(arr[0]);
+    Node* mover = head;
+
+
+    for(int i=1 ; i<arr.size() ; i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+
+    return head;
+}
+
+
+
+
+int main()
+{
+
+        vector<int> arr ={69,5,8,7};
+
+        Node *head = ConvertArrtoLL(arr);
+
+        cout << "Head is: " << head->data << endl;
+
+
+
+}
