c59b46ad4035bd999be3392b08dfd9cc19ec14b3
Aryan Salaria <arayn.32@gmail.com>
2025-03-29 22:43:25 +0530

    DP 1

---
+++ DP/maxprod.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr = {1,2,3,0,-1,-2,5,4,3,0,2,3,4,-1,-1,2};
+
+    int pre = 0;
+    int suf = 0;
+
+    int maxp = INT_MIN;
+
+    int pprod = 1;
+    int sprod = 1;
+
+    int n = arr.size();
+
+    for(int i=0 ; i<arr.size() ; i++)
+    {
+        if(pprod == 0)
+        {
+            pprod = 1;
+        }
+
+        if(sprod == 0)
+        {
+            sprod = 1;
+        }
+
+        pprod *= arr[i];
+        sprod *= arr[n-i-1];
+
+        maxp = max(maxp,max(sprod,pprod));
+    }
+
+    cout << maxp << endl;
+
+
+
+
+
+}
