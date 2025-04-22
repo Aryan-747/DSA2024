46d7d170476cf65a8299bef0042ff27a9b536f19
Aryan Salaria <arayn.32@gmail.com>
2025-04-22 22:44:11 +0530

    LL3

---
+++ LinkedList/LL3.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+
+Node* arrtoll(vector<int> array)
+{
+
+    if(array.size() == 0)
+    {
+        return nullptr;
+    }
+
+    Node* head = new Node(array[0]);
+    Node* mover = head;
+    for(int i=1 ; i<array.size() ; i++)
+    {
+        Node* temp = new Node(array[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+
+}
+
+void print(Node* head)
+{
+
+    Node* temp = head;
+
+    while(temp!=nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node* deletehead(Node* head)
+{
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+
+    return head;
+}
+
+
+int main()
+{
+
+    vector<int> array = {10,19,25,43,123};
+    Node* head = arrtoll(array);
+    head = deletehead(head);
+    print(head);
+
+}
