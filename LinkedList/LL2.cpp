5ffed3d15c90153a44f8ec56be807d88cde1a1cc
Aryan Salaria <arayn.32@gmail.com>
2025-06-13 23:50:05 +0530

    Many TC LL

--- LinkedList/LL2.cpp
+++ LinkedList/LL2.cpp
@@ -61,12 +61,81 @@ int lengthofLL(Node* head)
     return len;
 }
 
+Node* insert(Node *head, int val, int pos)
+{
+
+    Node* temp = head;
+    Node* prev = nullptr;
+    int cnt = 1;
+
+    int len = lengthofLL(head);
+
+    //inserting at begining
+    if(pos == 1)
+    {
+        Node* newnode = new Node(val);
+        newnode->next = head;
+        head = newnode;
+        return head;
+
+    }
+
+    // inserting at an index larger than the number of nodes present, Inserts new node at the end by default, irrespective of how large the insert position is
+    if(pos>len)
+    {
+        Node* newnode = new Node(val);
+
+        while(temp->next!=nullptr)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newnode;
+        len = lengthofLL(head);
+
+        return head;
+    }
+
+    // edgecase head is null
+    if(temp == nullptr)
+    {
+        Node* newnode = new Node(val);
+        
+        temp = newnode;
+        return temp;
+
+    }
+
+    // normal case
+    while(temp!=nullptr)
+    {
+
+        if(cnt == pos)
+        {
+            break;
+        }
+        
+        cnt++;
+        prev = temp;
+        temp = temp->next;
+        
+    }
+
+    Node* newnode = new Node(val);
+    prev->next = newnode;
+    newnode->next = temp;
+    return head;
+}
+
 
 int main()
 {
     vector<int> array = {69,2,3,4,5};
 
     Node* head = convertArrtoLL(array);
+    traversal(head);
+    cout << "length: " << lengthofLL(head) << "\n";
+    head = insert(head,55,5);
     traversal(head);
     cout << "length: " << lengthofLL(head) << "\n";
 
